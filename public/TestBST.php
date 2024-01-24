45947be980e61ffad42fda75830f71983912ea0b
MinhDuc2k03 <149854442+MinhDuc2k03@users.noreply.github.com>
2024-01-24 17:29:14 +0700

    fixed

--- public/TestBST.php
+++ public/TestBST.php
@@ -3,22 +3,22 @@ include "../vendor/autoload.php";
 use Dell\PhpDataStructureAndAlgorithms\DataStructures\BinarySearchTree;
 
 
-// Instantiate a new tree with root node of 5
+// Khởi tạo một tree với root bằng 5
 $bst = new BinarySearchTree(5);
 
-// Insert left branch nodes
+// Thêm nút cho branch trái
 $bst->insert(2);
 $bst->insert(1);
 $bst->insert(4);
 
-// Insert right branch nodes
+// Thêm nút cho branch phải
 $bst->insert(11);
 $bst->insert(7);
 $bst->insert(23);
 $bst->insert(16);
 $bst->insert(34);
 
-// Walk the tree
+// "Walk the tree"
 $tree = $bst->walk();
 foreach($tree as $node) {
     echo "{$node->value}" . PHP_EOL;
